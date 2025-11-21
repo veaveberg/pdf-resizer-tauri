ff6bcb16b651261427375f1230e32c477070c7ce
sasha <sasha@Mac.lan>
2025-11-21 23:18:16 +0400

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pdf-resizer-tauri/",
+  base: "/pdf-resizer/",
   plugins: [react(), svgr()],
   publicDir: "./public",
   build: {
